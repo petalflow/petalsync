430e7d703d7da61813dbed81f07d920e58eb6250
claytonlovin <luvincstart@gmail.com>
2023-07-05 14:13:42 -0300

    add logs

--- convutell/app.py
+++ convutell/app.py
@@ -24,7 +24,6 @@ app.add_middleware(
 
 
 from views.project import project
-from views.project import projectview
 from views.connection import connection
 from views.query import query
 from views.log import log
@@ -32,7 +31,6 @@ from views.time import time
 from views.ds_type_query import _type
 
 app.include_router(project.router)
-app.include_router(projectview.router)
 app.include_router(connection.router)
 app.include_router(query.router)
 app.include_router(log.router)
